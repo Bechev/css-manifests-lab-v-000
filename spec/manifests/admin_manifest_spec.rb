483a260623fdb16ee3b54e1adfce7734f50800a4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-26 17:00:33 +0000

    automatically backed up by learn

--- spec/manifests/admin_manifest_spec.rb
+++ spec/manifests/admin_manifest_spec.rb
@@ -4,15 +4,15 @@ RSpec.describe do
   # it "should not require the posts file in the admin manifest" do
   #   expect(File.read("app/assets/stylesheets/admin.css")).not_to include("*= require posts")
   # end
-  # 
+  #
   # it "should require the bootstrap-addon file in the admin manifest" do
   #   expect(File.read("app/assets/stylesheets/admin.css")).to include("*= require bootstrap-addon")
   # end
-  # 
+  #
   # it "should require the jquery-addon file in the admin manifest" do
   #   expect(File.read("app/assets/stylesheets/admin.css")).to include("*= require jquery-addon")
   # end
-  # 
+  #
   # it "should require the blogs file in the admin manifest" do
   #   expect(File.read("app/assets/stylesheets/admin.css")).to include("*= require blogs")
   # end
