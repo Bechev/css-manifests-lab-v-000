7ece0576177bb318d5e0d9ae9328374543bad31b
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-26 17:00:19 +0000

    automatically backed up by learn

--- spec/layouts/admin_layout_spec.rb
+++ spec/layouts/admin_layout_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe do
   # it "should include the main file in the admin layout" do
   #   expect(File.read("app/views/layouts/admin.html.erb")).to match(/stylesheet_link_tag ['"]admin['"]/)
   # end
-  # 
+  #
   # it "should include the remaining CSS file (posts.css) in the admin layout" do
   #   expect(File.read("app/views/layouts/admin.html.erb")).to match(/stylesheet_link_tag ['"]posts['"]/)
   # end
